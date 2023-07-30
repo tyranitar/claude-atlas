6125f2584c697d14ba81353aafa3a809278e6e49
Tyron Jung <37984135+tyranitar@users.noreply.github.com>
2023-07-29 21:35:24 -0700

    Add CORS

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from api.api import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
